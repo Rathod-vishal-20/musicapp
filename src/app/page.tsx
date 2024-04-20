7bf3dc82f15ff95b1ff41eaa5484fc95672bef59
Vishal Rathod <vishalrathod191918@gmail.com>
2024-04-20 14:16:01 +0530

    Uploading HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased
     bg-grid-white/[0.02]">
-      <div className="text-4xl text-orange-500 text-center font-serif font-bold"> Chai aur code</div>
+
+      <HeroSection/>
     </main>
   );
 }
